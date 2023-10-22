c4bfd240b6f228c6cfe3b0be3f0ad426a9a6810b
Tasnia Shahrin <tasniashahrin269@gmail.com>
2023-10-22 09:54:07 +0600

    initial commit

--- OJOQuickTalk/app/src/main/java/com/example/ojoquicktalk/TabsAccessorAdapter.java
+++ OJOQuickTalk/app/src/main/java/com/example/ojoquicktalk/TabsAccessorAdapter.java
@@ -32,11 +32,9 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
             case 2:
                 ContactsFragment contactsFragment = new ContactsFragment();
                 return contactsFragment;
-
-//            case 3:
-//                RequestsFragment requestsFragment = new RequestsFragment();
-//                return requestsFragment;
-
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
             default:
                 return null;
         }
@@ -46,7 +44,7 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
 
     public int getCount()
     {
-        return 3;
+        return 4;
     }
 
 
@@ -66,10 +64,8 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
 
             case 2:
                 return "Contacts";
-
-//            case 3:
-//                return "Requests";
-
+            case 3:
+                return "Requests";
             default:
                 return null;
         }
